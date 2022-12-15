43daf51085f0e9505dca88e634b72f8745338fae
Adei Dsoo <dsoo@MacBook-Air-de-Adei.local>
2022-12-14 19:44:18 -0600

    style newNote component

--- lab-notes/src/auth/Home.jsx
+++ lab-notes/src/auth/Home.jsx
@@ -6,29 +6,32 @@ import './HomePage/home.css';
 
 export const Home = ({ user }) => {
     const [showNewNote, setShowNewNote] = useState(false)
-    const[noNotes, SetNoNotes]=useState(true)
+    const [noNotes, setNoNotes] = useState(true)
+    // AGREGAR Iconos de libreria imagenes, add, search
     //dos funciones crear nota y otra de usuario
+
     console.log(user);
 
-const newNote =()=>{
-
-}
+    const newNote = () => {
+        setNoNotes(false)
+        setShowNewNote(true)
+    }
 
     return (
         <div className="home">
             <section className="searchAndCreateNote">
                 <div className="buttonAndSearch">
-                    <button>CREAR NOTA +</button>
+                    <button onClick={() => { newNote() }}>CREAR NOTA   +</button>
                     <input className='search' type="text" />
                 </div>
                 <section className="notes">
-                    <OneNote />
-                    {/* <div className='showNote'></div> */}
+                    {showNewNote && <OneNote />}
                 </section>
             </section>
 
+            {noNotes && <NoNotes />}
             <Exit />
-            <NoNotes />
+
         </div>
     )
 }
